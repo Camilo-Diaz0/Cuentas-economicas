136564e78af6797cf3363320ef5797b6b5b4d9bc
Camilo Diaz <diazc.pr7966@gmail.com>
2023-02-16 17:39:28 -0500

    añadiendo una interfaz de usuario basica sin funcionalidad aun

--- src/cuentasPersonales/Main.java
+++ src/cuentasPersonales/Main.java
@@ -1,11 +1,12 @@
 package cuentasPersonales;
 
-import cuentasPersonales.entities.Usuario;
-import cuentasPersonales.servicios.UsuariosServices;
+import cuentasPersonales.swing.CuentaFrame;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+/*        System.out.println("Hello world!");
         UsuariosServices userService = new UsuariosServices();
         userService.registrarUsuario(new Usuario("Camilo","Diaz",120000));
         userService.registrarUsuario(new Usuario("Claudia","Zapata",3500000));
@@ -15,6 +16,13 @@ public class Main {
         userService.actualizarDinero("camilo", 75000);
         userService.eliminarUsuario("jurany");
         userService.listarUsuarios();
-        System.out.println((userService.obtenerUsuario("camilo")));
+        System.out.println((userService.obtenerUsuario("camilo")));*/
+        JFrame frame = new JFrame("swingInter");
+        frame.setContentPane(new CuentaFrame().panelPrincial);
+        frame.setVisible(true);
+        frame.setSize(450,450);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
---
+++ src/cuentasPersonales/swing/CuentaFrame.java
@@ -0,0 +1,21 @@
+package cuentasPersonales.swing;
+
+import javax.swing.*;
+
+public class CuentaFrame {
+    public JPanel panelPrincial;
+    private JLabel nombre1;
+    private JLabel nombre2;
+    private JLabel nombre3;
+    private JLabel dinero1;
+    private JLabel dinero2;
+    private JLabel dinero3;
+    private JPanel sum1;
+    private JPanel restar1;
+    private JPanel sum2;
+    private JPanel sum3;
+    private JPanel restar2;
+    private JPanel restar3;
+
+    public CuentaFrame(){}
+}
